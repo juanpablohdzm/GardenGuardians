bd5e8716cb965e23c6d41d6ce10fc01e4824ce01
DESKTOP-1MK8LI7\Juan Pablo <juanpablohdzm95@gmail.com>
2017-07-23 18:12:12 -0500

    Alterantive Music Manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+    public float AutoLoadLevelTime;
+
+    private void Start()
+    {
+        //Delay for splash screen
+        Invoke("LoadNextLevel", AutoLoadLevelTime);
+       
+    }
+    public void LoadLevel(string name)
+    { 
+		SceneManager.LoadScene (name);
+	}
+
+	public void QuitRequest()
+    {
+		Application.Quit ();
+	}
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+       
+    }
+
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour {
+    public AudioClip[] LevelMusicChangeArray;
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        
+    }
+
+    private void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.clip = LevelMusicChangeArray[0];
+        audioSource.Play();
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        AudioClip ThisLevelClip = LevelMusicChangeArray[level];
+        if (ThisLevelClip != null)
+        {
+            audioSource.clip = ThisLevelClip;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+}
