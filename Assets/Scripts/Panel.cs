449b3e811bd43c7ffe623d11ffbbb8de0f215fea
DESKTOP-1MK8LI7\Juan Pablo <juanpablohdzm95@gmail.com>
2017-07-24 07:24:31 -0500

    Levels and Fade transitions

---
+++ Assets/Scripts/Panel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Panel : MonoBehaviour {
+
+    public float FadeInTime;
+	// Use this for initialization
+	void Start ()
+    {
+        GetComponent<Image>().CrossFadeAlpha(0, FadeInTime, true);
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Time.timeSinceLevelLoad >= FadeInTime)
+            gameObject.SetActive(false);
+    }
+}
