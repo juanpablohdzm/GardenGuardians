ea5190c956ca3a240e8f0bfbb0aa4a5daa8b1f7d
DESKTOP-1MK8LI7\Juan Pablo <juanpablohdzm95@gmail.com>
2017-07-25 12:18:37 -0500

    Anim Function Call

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -2,14 +2,32 @@
 
 public class Attacker : MonoBehaviour {
 
-    [Range (0,2f)] public float WalkSpeed;
+    [Range (0,2f)] public float CurrentSpeed;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        Rigidbody2D RigidBody =gameObject.AddComponent<Rigidbody2D>();
+        RigidBody.isKinematic = true;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        transform.Translate(Vector2.left * WalkSpeed * Time.deltaTime);
+	void Update ()
+    {
+        transform.Translate(Vector2.left * CurrentSpeed * Time.deltaTime);
 	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print(collision.name);
+    }
+
+    public void SetCurrentSpeed(float Speed)
+    {
+        CurrentSpeed = Speed;
+    }
+
+    public void StrikeCurrentTarget(float Damage)
+    {
+
+    }
 }
