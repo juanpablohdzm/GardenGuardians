5d0f56315b53f2391982d744f601e6eed122e69b
DESKTOP-1MK8LI7\Juan Pablo <juanpablohdzm95@gmail.com>
2017-07-25 16:14:59 -0500

    Animating Defenders and projectiles

---
+++ Assets/Scripts/projectile.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class projectile : MonoBehaviour {
+
+    public float Speed=1f;
+    public float Damage=10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.right * Speed * Time.deltaTime);
+	}
+}
