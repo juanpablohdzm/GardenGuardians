343bc4d741bd94de84b80bf5b8a4b9e30f24a6e2
DESKTOP-1MK8LI7\Juan Pablo <juanpablohdzm95@gmail.com>
2017-07-25 19:27:06 -0500

    Tower Selector Buttons

---
+++ Assets/Button.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    private Button[] buttonArray;
+    public GameObject DefenderPrefab;
+
+    public static GameObject SelectDefender;
+    // Use this for initialization
+    void Start()
+    {
+        buttonArray = GameObject.FindObjectsOfType<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    private void OnMouseDown()
+    {
+        for (int i = 0; i < buttonArray.Length; i++)
+        {
+            if (buttonArray[i].gameObject == GetComponent<Button>().gameObject)
+            {
+               
+                GetComponent<SpriteRenderer>().color = Color.white;
+                SelectDefender = DefenderPrefab;
+            }
+            else
+            {
+                
+                buttonArray[i].GetComponent<SpriteRenderer>().color = Color.black;
+            }
+           
+
+        }
+        
+    }
+   
+}
