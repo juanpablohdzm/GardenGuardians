847379fcb2a295633e8468e84b816767afe4673f
DESKTOP-1MK8LI7\Juan Pablo <juanpablohdzm95@gmail.com>
2018-11-12 16:28:10 -0600

    Add more options for better user experience (screen resolution change, quality settings, fullscreen mode)

--- Assets/Scripts/OptionsController.cs
+++ Assets/Scripts/OptionsController.cs
@@ -1,21 +1,41 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
 
 public class OptionsController : MonoBehaviour {
 
     public Slider VolumeSlider;
     public Slider DifficultySlider;
+    public Dropdown dropdownResultions;
 
     public float DefaultVolumeValue;
     public float DefaultDifficultyLevel;
 
     private MusicManager musicManager;
+    private Resolution[] screenResolution;
+    private bool bIsFullScreen=true;
+
 	// Use this for initialization
 	private void Start ()
     {
         musicManager = GameObject.FindObjectOfType<MusicManager>();
         VolumeSlider.value = PlayerPrefsManager.GetMasterVolume();
         DifficultySlider.value = PlayerPrefsManager.GetLevelDifficulty();
+
+        screenResolution = Screen.resolutions;
+        List<string> resolutions = new List<string>();
+        int index = 0;
+        dropdownResultions.ClearOptions();
+        for (int i = 0; i < screenResolution.Length; i++)
+        {
+            resolutions.Add(screenResolution[i].ToString());
+            if (screenResolution[i].width == Screen.currentResolution.width && screenResolution[i].height == Screen.currentResolution.height)
+                index = i;
+
+        }
+        dropdownResultions.AddOptions(resolutions);
+        dropdownResultions.value = index;
 	}
 
     private void Update()
@@ -25,13 +45,34 @@ public class OptionsController : MonoBehaviour {
 
     public void SafeAndExit()
     {
-        PlayerPrefsManager.SetMasterVolume(VolumeSlider.value);
+
         PlayerPrefsManager.SetLevelDifficulty(DifficultySlider.value);
     }
 
     public void Defaults()
     {
-        VolumeSlider.value = DefaultVolumeValue;
+        OnChangeVolume(DefaultVolumeValue);
         DifficultySlider.value = DefaultDifficultyLevel;
+    }
+
+    public void OnChangeVolume(float value)
+    {
+        PlayerPrefsManager.SetMasterVolume(value);
+    }
+
+    public void OnFullScreenChanged()
+    {
+        bIsFullScreen = !bIsFullScreen;
+        Screen.fullScreen = bIsFullScreen;
+    }
+
+    public void OnScreenResolutionChanged(int index)
+    {
+        Screen.SetResolution(screenResolution[index].width, screenResolution[index].height, bIsFullScreen);
+    }
+
+    public void OnQualityChanged(int index)
+    {
+        QualitySettings.SetQualityLevel(index);
     }
 }
